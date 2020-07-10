9744d045fcb1081ecfb1aff39c36e5ca8f3cccb7
Gaspard Ferey <gaspard.ferey@inria.fr>
2020-07-10 10:16:42 +0200

    WIP

--- __init__.py
+++ __init__.py
@@ -4,7 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import routes
+@app.route("/")
+def hello():
+    return "Hello world!"
 
 if __name__ == "__main__":
     app.run(debug=True)
