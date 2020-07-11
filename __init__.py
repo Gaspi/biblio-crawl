c96c9bf6b45c77dee8feea1c82584752fb58ea1c
Gaspard FEREY <gaspard.ferey@hotmail.fr>
2020-07-11 20:16:27 +0200

    WIP

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,6 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello world!"
+
+
+def test():
+   return "test"
+app.add_url_rule("/test", "test", test)
 
 if __name__ == "__main__":
     app.run(debug=True)
